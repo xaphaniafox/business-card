9273a44d9b4b12896c2fb540ac8f2208e1a18ff8
xaphaniafox <f.hosseini64@gmail.com>
2023-02-14 16:31:58 +0300

    adding Info component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import Info from './components/Info'
 
 function App() {
   return (
     <div className="App">
-      
+      <Info />
     </div>
   )
 }
