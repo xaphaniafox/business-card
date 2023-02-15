1290a12946e93199f33dd0643d09ae71d04b10c9
xaphaniafox <f.hosseini64@gmail.com>
2023-02-15 21:32:39 +0300

    completing the Info component

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,13 +3,13 @@ import '../assets/css/info-style.css'
 import InfoPic from '../assets/img/info-pic.jpg'
 const Info = () => {
   return (
-    <>
+    <div className='info'>
       <img src={InfoPic} className="info--pic" alt="a picture of me" />
       <h2 className='info--name'>Fatemeh Hosseini</h2>
       <h4 className='info--jobtitle'>Frontend Developer</h4>
       <h5 className='info--website'>f.hosseini64@gmail.com</h5>
-      <button type='button'>Email</button>
-    </>
+      <button className='info--btn' type='button'>Email</button>
+    </div>
   )
 }
 export default Info
