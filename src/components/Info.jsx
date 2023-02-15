7c122642e0f55d29146addbf113143b0d433d13f
xaphaniafox <f.hosseini64@gmail.com>
2023-02-15 10:10:35 +0300

    correcting the className

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -7,7 +7,7 @@ const Info = () => {
       <img src={InfoPic} className="info--pic" alt="a picture of me" />
       <h2 className='info--name'>Fatemeh Hosseini</h2>
       <h4 className='info--jobtitle'>Frontend Developer</h4>
-      <h5 className='info--email'>f.hosseini64@gmail.com</h5>
+      <h5 className='info--website'>f.hosseini64@gmail.com</h5>
       <div className="buttons">
         <button type='button'>Email</button>
         <button type='button'>LinkedIn</button>
